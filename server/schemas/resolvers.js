dad115ee7864cb1bbf44c623ddf9f029bc7f042f
BRIAN MAHER <bmaher22@mac.comclear>
2023-11-21 16:52:48 -0500

    still need to deploy to heroku

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,73 +2,76 @@ const { User } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                return User.findOne({ _id: context.user._id });
-            }
-            throw AuthenticationError;
-        },
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
     },
-    Mutation: {
-        addUser: async (parent, { username, email, password }) => {
-            const user = await User.create({ username, email, password });
-            const token = signToken(user);
-            return { token, user };
-          },
-          login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
-      
-            if (!user) {
-              throw AuthenticationError;
-            }
-      
-            const correctPw = await user.isCorrectPassword(password);
-      
-            if (!correctPw) {
-              throw AuthenticationError;
-            }
-      
-            const token = signToken(user);
-      
-            return { token, user };
-          }, catch (err) {
-            console.log(err);
-            throw new Error("Internal server error");
-          }
-        },
-        saveBook: async (_, { input }, { user }) => {
-          try {
-            const updatedUser = await User.findOneAndUpdate(
-              { _id: user._id },
-              { $addToSet: { savedBooks: input } },
-              { new: true, runValidators: true }
-            );
-    
-            return updatedUser;
-          } catch (err) {
-            console.log(err);
-            throw new Error("Internal server error");
-          }
-        },
-        deleteBook: async (_, { bookId }, { user }) => {
-          try {
-            const updatedUser = await User.findOneAndUpdate(
-              { _id: user._id },
-              { $pull: { savedBooks: { bookId } } },
-              { new: true }
-            );
-    
-            if (!updatedUser) {
-              throw new Error("Couldn't find user with this id!");
-            }
-    
-            return updatedUser;
-          } catch (err) {
-            console.log(err);
-            throw new Error("Internal server error");
-          }
-        },
-      };
-    
-    module.exports = resolvers;
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw AuthenticationError;
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw AuthenticationError;
+        }
+
+        const token = signToken(user);
+
+        return { token, user };
+      } catch (err) {
+        console.log(err);
+        throw new Error("Internal server error");
+      }
+    },
+    saveBook: async (_, { bookInput }, { user }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: bookInput } },
+          { new: true, runValidators: true }
+        );
+
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+        throw new Error("Internal server error");
+      }
+    },
+    removeBook: async (_, { bookId }, { user }) => {
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
+        if (!updatedUser) {
+          throw new Error("Couldn't find user with this id!");
+        }
+
+        return updatedUser;
+      } catch (err) {
+        console.log(err);
+        throw new Error("Internal server error");
+      }
+    },
+  }
+
+};
+
+module.exports = resolvers;
